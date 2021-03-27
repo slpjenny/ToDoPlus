4aa86c7f8befb08f294a4e419ff9ae3bbd5d67b5
Ji Young <slpjenny@naver.com>
2021-03-27 15:34:43 +0900

    view pager & bottom navigation 연동

--- app/src/main/java/com/mytest/todoplus/MainActivity.java
+++ app/src/main/java/com/mytest/todoplus/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mytest.todoplus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -7,10 +8,20 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    Calendar_Fragment calendar_fragment;
+    MainFragment main_fragment;
+    Routine_Fragment routine_fragment;
+
+    ViewPager pager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +35,57 @@ public class MainActivity extends AppCompatActivity {
         //getSupportFragmentManager로 프래그먼트 참조가능
         MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager());
 
-        Calendar_Fragment calendar_fragment= new Calendar_Fragment();
+        //뷰 페이저의 1번째 페이지='달력'
+        calendar_fragment= new Calendar_Fragment();
         adapter.addItem(calendar_fragment);
 
-        MainFragment main_fragment = new MainFragment();
+        //뷰 페이저의 2번째 페이지='투두'(메인)
+        main_fragment = new MainFragment();
         adapter.addItem(main_fragment);
 
-        Routine_Fragment routine_fragment = new Routine_Fragment();
+        //뷰 페이저의 3번째 페이지='루틴'
+        routine_fragment = new Routine_Fragment();
         adapter.addItem(routine_fragment);
 
         pager.setAdapter(adapter);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.tab1:
+                        pager.setCurrentItem(0);
+                        return true;
+
+                    case R.id.tab2:
+                        pager.setCurrentItem(1);
+                        return true;
+
+                    case R.id.tab3:
+                        pager.setCurrentItem(2);
+                        return true;
+                }
+                return false;
+            }
+        });
+
+//        pager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {
+//                bottomNavigationView.getMenu().getItem(position).isChecked();
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
     }
 
     //어댑터 안에서 각각의 아이템(프라그먼트 페이지들)을 데이터로서 관리한다
@@ -58,5 +110,6 @@ public class MainActivity extends AppCompatActivity {
             return items.size();
         }
     }
+
 
 }
