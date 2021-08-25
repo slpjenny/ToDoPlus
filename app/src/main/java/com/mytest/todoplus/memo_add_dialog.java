e6b82e3e5999f95bac7e7471b4d5b6fe88bb3196
eunbicho <puppy101976@naver.com>
2021-08-26 01:23:48 +0900

    feat: saving one memo per day

--- app/src/main/java/com/mytest/todoplus/memo_add_dialog.java
+++ app/src/main/java/com/mytest/todoplus/memo_add_dialog.java
@@ -2,8 +2,10 @@ package com.mytest.todoplus;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -41,6 +43,8 @@ public class memo_add_dialog extends Dialog {
         db = helper.getWritableDatabase();
         helper.onCreate(db);
 
+        Cursor c = db.query("mymemo", null, null, null, null, null, null, null);
+
         memoName = findViewById(R.id.memoName);
         memoContent = findViewById(R.id.memoContent);
         memoAdd_ok = findViewById(R.id.memoAdd_ok);
@@ -60,18 +64,30 @@ public class memo_add_dialog extends Dialog {
                 String name = memoName.getText().toString();
                 String content = memoContent.getText().toString();
 
-                //제목 or 내용 입력하지 않았을 때
+                // 제목 or 내용 입력하지 않았을 때
                 if (name == null || name.equals("")) {
                     Toast.makeText(getContext(), "제목을 입력해 주세요.", Toast.LENGTH_SHORT).show();
+
                 } else if (content == null || content.equals("")) {
                     Toast.makeText(getContext(), "메모를 입력해 주세요.", Toast.LENGTH_SHORT).show();
-                } else {
-                    //db에 저장
-                    helper.insertMemo(name,content,getTime(),100);
-                    Toast.makeText(getContext(), "저장되었습니다", Toast.LENGTH_SHORT).show();
-                    dismiss();
-                }
 
+                // 제목과 내용 모두 입력했을 때
+                } else {
+                    c.moveToLast();
+                    // 같은 날짜에 해당하는 데이터가 있으면 저장 안 되게
+                    if(c.getString(c.getColumnIndex("date")).equals(getTime())){
+                        Toast.makeText(getContext(), "이미 저장된 메모가 있어요!", Toast.LENGTH_SHORT).show();
+                        dismiss();
+                    }
+                    // 같은 날짜에 해당하는 데이터가 없으면 저장 되게
+                    else if (!c.getString(c.getColumnIndex("date")).equals(getTime())){
+                        // 오늘 저장한 메모 데이터가 없으면 db에 저장
+                        helper.insertMemo(name,content,getTime(),100);
+                        Toast.makeText(getContext(), "저장되었습니다", Toast.LENGTH_SHORT).show();
+                        dismiss();
+                    }
+
+                }
             }
         });
 
