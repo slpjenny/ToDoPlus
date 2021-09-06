df61d275a6cd378a36fa9b5912af7ed637e4ab72
eunbicho <puppy101976@naver.com>
2021-09-06 11:05:51 +0900

    feat: Add small dot in calendar

--- app/src/main/java/com/mytest/todoplus/CalendarViewHolder.java
+++ app/src/main/java/com/mytest/todoplus/CalendarViewHolder.java
@@ -1,6 +1,7 @@
 package com.mytest.todoplus;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -9,11 +10,13 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
     public final TextView dayOfMonth;
+    public final ImageView dot;
     private final CalendarAdapter.OnItemListener onItemListener;
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
     {
         super(itemView);
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        dot = itemView.findViewById(R.id.dot);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
     }
